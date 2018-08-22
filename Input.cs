6d4bc6faf9eefb17977f5148b313621bc28e5cb2
Semenihin <37003032+Semenihin@users.noreply.github.com>
2018-08-22 17:22:05 +0300

    Create Input.cs

---
+++ Input.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pcar21.Models
+{
+    public class Input
+    {
+        public string Id { get; set; }
+        public string PowerConsumption { get; set; }
+        public string ReplacementCost { get; set; }
+    }
+}
