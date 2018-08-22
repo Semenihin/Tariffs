1ea3124760308e913dd8a2e1b0dbb4f3871e4629
Semenihin <37003032+Semenihin@users.noreply.github.com>
2018-08-22 17:22:32 +0300

    Create Output.cs

---
+++ Output.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pcar21.Models
+{
+    public class Output
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
